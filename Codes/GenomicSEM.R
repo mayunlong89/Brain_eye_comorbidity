b0fb3c84848cfc4171b79b21e99f8de80eb4a3f8
Yunlong Ma <11418032@zju.edu.cn>
2024-03-14 14:45:34 -0400

    Update GenomicSEM.R

--- Codes/GenomicSEM.R
+++ Codes/GenomicSEM.R
@@ -6,7 +6,7 @@ library(ggtext)
 library(wesanderson)
 library(tidyr)
 library(reshape2)  
-#下载GenomicSEM
+#Downlaod and install GenomicSEM
 install.packages("devtools")
 library(devtools)
 install_github("GenomicSEM/GenomicSEM")
@@ -14,10 +14,10 @@ require(GenomicSEM)
 setwd("/share/pub1/gouxx/gouxx/GWAS/brain_eye_sumstats/")
 datapath="/share/pub1/gouxx/gouxx/GWAS/brain_eye_sumstats/"
 gwas_list<-list.files(path=datapath,pattern = ".sumstats.gz")
-#运行多变量LDSC
+#running LDSC
 traits<-gwas_list
-sample.prev<-c(.172,.234,.397,.101,.346,.276,.337,.407,.088,.027,.115,.436,.054)#样本患病率
-population.prev<-c(.05,.01,.01,.01,.15,.025,.008,.01,.123,.0176,.0251,.306,.0002)#人口患病率
+sample.prev<-c(.172,.234,.397,.101,.346,.276,.337,.407,.088,.027,.115,.436,.054)  #(样本患病率)
+population.prev<-c(.05,.01,.01,.01,.15,.025,.008,.01,.123,.0176,.0251,.306,.0002) #(人口患病率)
 ld<-"/share2/pub/jiangdp/jiangdp/ldsc-master/eur_w_ld_chr/"
 wld<-"/share2/pub/jiangdp/jiangdp/ldsc-master/eur_w_ld_chr/"
 trait.names<-c("ADHD","AN","ASD","BIP","MDD","OCD","TS","SCZ","AMD","DR","GLC","Myopia","RD")
